013d289718be497d2cda90bb62c00062dc4743c6
sana <ss3112361@gmail.com>
2024-11-05 23:32:39 +0530

    first commit

---
+++ src/main/java/com/example/DevApp.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class DevApp {
+    public static void main(String[] args) {
+        System.out.println("Running in development environment!");
+    }
+
+    
+}
---
+++ src/main/java/com/example/ProdApp.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class ProdApp {
+    public static void main(String[] args) {
+        System.out.println("Running in production environment!");
+    }
+    
+}
